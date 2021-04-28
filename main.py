63e777147e0c45a5ba6a16ea8d6d7913aa6be14c
Nathanial Johnson <nathanial.r.johnson@gmail.com>
2021-04-28 14:09:45 -0500

    completed the get_winner function in sweepstakes

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from sweepstake import Sweepstake
 from marketing_firm import MarketingFirm
 from marketing_firm_creator import MarketingFirmCreator
-from sweepstakes_queue_manager import SweepstakesQueueManager
-from sweepstakes_stack_manager import SweepstakesStackManager
 
 
 def run_simulation():
     sweepstake = Sweepstake("Test_sweepstake")
     sweepstake.register_contestant()
+    sweepstake.register_contestant()
+    sweepstake.register_contestant()
+    winner = sweepstake.pick_winner()
+    print(winner.first_name)
+    print(winner.last_name)
+    print(winner.email_address)
     marketing_firm_creator = MarketingFirmCreator()
     marketing_firm = MarketingFirm(marketing_firm_creator.choose_manager_type())
     test = marketing_firm.create_sweepstakes("Test")
     marketing_firm.manager.insert_sweepstakes(test)
-    
+
+
 
 run_simulation()
--- sweepstake.py
+++ sweepstake.py
@@ -1,5 +1,6 @@
 import user_interface as ui
 from contestant import Contestant
+import random
 
 
 class Sweepstake:
@@ -8,11 +9,17 @@ class Sweepstake:
         self.contestants = {}
 
     def register_contestant(self):
-        ui.new_contestant(Contestant())
+        contestant = ui.new_contestant(Contestant())
+        if contestant.registration_number in self.contestants.keys():
+            self.register_contestant()
+        self.contestants[contestant.registration_number] = contestant
 
     def pick_winner(self):
-        #return contestant
-        pass
+        keys = list(self.contestants)
+        winner = random.randrange(0, len(keys))
+        winner = keys[winner]
+        print(winner)
+        return self.contestants.get(winner)
 
     def print_contestant_info(self, contestant):
         print(contestant.first_name)
